bcb69a9a53d2714c6f73bd95363105f9ace67f0a
Rafal Szalanski <rafal.szalanski@gmail.com>
2016-09-25 19:40:28 +0100

    Better json structure

--- src/code_miner/core.clj
+++ src/code_miner/core.clj
@@ -18,15 +18,19 @@
        (group-by (fn [coupling-vec] (first coupling-vec)))))
 
 
-(defn file-couplings-map [m]
+(defn file-couplings-map
+  "Input: a map with the structure described below. Output: a vector of maps described below."
+  ;; INPUT { filename1 [[related-file1 coupling1 revisions1] ... [related-fileN couplingN revisions N]] }
+  ;; OUTPUT [{:file filename1 :couplings [{:file related-filename1 :coupling-level .. :revisions ..}]}]
+  [m]
   (let [f (fn [[file coupling revisions]]
-            { :file file :coupling coupling :revisions revisions})
+            { :file file :coupling-level coupling :revisions revisions})
         g (fn [couplings] (into [] (map #(f (rest %1)) couplings)))]
-    (into {} (for [[k v] m] [k (g v)]))))
+    (into [] (for [[k v] m] {:file k :couplings (g v) }))))
 
 (def jsn (->> csv-data
      (grouped-coupling)
      (file-couplings-map)))
 
 (with-open [w (io/writer "coupling.json" :append true)]
-  (.write w (json/write-str jsn)))
+  (.write w (json/write-str { :data jsn })))
